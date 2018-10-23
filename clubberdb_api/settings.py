ca332f17991d1e4b04f7621912755a5d0676c06b
jgabaja <jgabaja@gmail.com>
2018-10-23 12:17:53 +0800

    changed settings.py

--- clubberdb_api/settings.py
+++ clubberdb_api/settings.py
@@ -130,6 +130,6 @@ STATIC_URL = '/static/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+#STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, 'static'),
+#)
