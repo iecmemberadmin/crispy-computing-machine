3c1f404cca2ca0cc84c10729fbf62010657b5703
jgabaja <jgabaja@gmail.com>
2018-10-23 17:08:40 +0800

    wip

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from . models import Clubber
+from . models import Clubber, ClubberDetail
 
 # Register your models here.
 admin.site.register(Clubber)
+admin.site.register(ClubberDetail)
