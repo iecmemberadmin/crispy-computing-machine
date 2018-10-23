b53bc400900b6d12644f1dd154d9124ffd4c2577
jgabaja <jgabaja@gmail.com>
2018-10-23 15:34:40 +0800

    changed users/views.py

--- users/urls.py
+++ users/urls.py
@@ -3,8 +3,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from users import views
 
 urlpatterns = [
-  path('clubbers/', views.ClubberList.as_view()),
-  path('clubbers/<str:pk>/', views.ClubberDetail.as_view())
+  path('clubbers/', views.ClubberList),
+  path('clubbers/<str:pk>/', views.ClubberDetail)
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
