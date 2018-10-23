a937bbad83fb24f495a3f8524083bc2747307395
jgabaja <jgabaja@gmail.com>
2018-10-23 15:21:21 +0800

    changed users/views.py

--- users/views.py
+++ users/views.py
@@ -1,13 +1,42 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .models import Clubber
 from .serializers import ClubberSerializer
 
 # Create your views here.
-class ClubberList(generics.ListCreateAPIView):
-  queryset = Clubber.objects.all()
-  serializer_class = ClubberSerializer
+@api_view(['GET', 'POST'])
+class ClubberList(request):
+  if request.method === 'GET':
+    clubbers = Clubber.objects.all()
+    serializer = ClubberSerializer(clubbers, many=True)
+    return Response(serializer.data)
+  elif request.method === 'POST':
+    serializer = ClubberSerializer(data=request.data):
+    if serializer.is_valid():
+      serializer.save()
+      return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class ClubberDetail(generics.RetrieveUpdateDestroyAPIView):
-  queryset = Clubber.objects.all()
-  serializer_class = ClubberSerializer
+@api_view(['GET', 'PUT', 'DELETE'])
+class ClubberDetail(request):
+  try:
+      clubber = Clubber.objects.get(student_number=request.data.student_number, password=request.data.password)
+  except Clubber.DoesNotExist:
+      return Response(status=status.HTTP_404_NOT_FOUND)
+
+  if request.method == 'GET':
+      serializer = ClubberSerializer(clubber)
+      return Response(serializer.data)
+
+  elif request.method == 'PUT':
+      serializer = ClubberSerializer(clubber, data=request.data)
+      if serializer.is_valid():
+          serializer.save()
+          return Response(serializer.data)
+      return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+  elif request.method == 'DELETE':
+      clubber.delete()
+      return Response(status=status.HTTP_204_NO_CONTENT)
